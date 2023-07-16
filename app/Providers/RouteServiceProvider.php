86514c7e7c1a29dbd6ecf4a28ed5c094ce36b400
FLEduc <frederic.laurantin.education@gmail.com>
2023-07-16 23:51:21 +0200

    MultiAuth - Routes + Provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,16 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['web', 'auth', 'role:admin'])
+                ->prefix('admin')
+                ->name('admin.')
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware(['web', 'auth', 'role:vendor'])
+                ->prefix('vendor')
+                ->name('vendor.')
+                ->group(base_path('routes/vendor.php'));
         });
     }
 }
